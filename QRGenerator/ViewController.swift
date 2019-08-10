50beb6701186373c0d19096702e42e87a09ef7ac
Ramcharan <ramcharan.gaddam@gmail.com>
2019-08-10 18:01:47 +0530

    QRCode Generator, initial commit

--- QRGenerator/ViewController.swift
+++ QRGenerator/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var yourQRCodeLabel: UILabel!
+    
+    @IBOutlet weak var qrcodeImageView: UIImageView!
+    
+    var qrImage:CIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.yourQRCodeLabel.isHidden = true
+        self.qrcodeImageView.isHidden = true
+        textField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text == "" { return false }
+        let data = textField.text?.data(using: .isoLatin1, allowLossyConversion: false)
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("M", forKey: "inputCorrectionLevel")
+        qrImage = filter?.outputImage
+        qrcodeImageView.image = UIImage(ciImage: qrImage!)
+        self.yourQRCodeLabel.isHidden = false
+        self.qrcodeImageView.isHidden = false
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
